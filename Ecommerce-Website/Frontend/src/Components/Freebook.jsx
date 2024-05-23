2eacfd9a60404a11c93971526a8989b0304e16ac
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-05-23 19:04:46 +0530

    update

---
+++ Ecommerce-Website/Frontend/src/Components/Freebook.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Freebook() {
+    function Freebook() {
+        const filterDate = list.filter((data) => data.category === "Free");
+        console.log(FilterData)
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Freebook
